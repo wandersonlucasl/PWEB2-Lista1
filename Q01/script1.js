66b74780aab97cc986c44d3d6a0dc68dec2af648
Wanderson Lucas <115495149+wandersonlucasl@users.noreply.github.com>
2022-10-12 14:50:34 -0300

    Add files via upload

---
+++ Q01/script1.js
@@ -0,0 +1,7 @@
+let x = parseInt(prompt("Digite um número "))
+while(x != 0){
+    document.write(x*x)
+    document.write("<br>")
+    x = parseInt(prompt("Digite mais um número. Para sair digite 0"))
+    
+}
